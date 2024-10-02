fd9ba072a57b6bc1649dc738b466e9c065d7718b
StoneCAU <seokwoo1999@naver.com>
2024-10-02 18:00:17 +0900

    feat: visitDay로 이벤트 적용여부 클래스 Event 생성

--- src/main/java/christmas/controller/MainController.java
+++ src/main/java/christmas/controller/MainController.java
@@ -1,7 +1,9 @@
 package christmas.controller;
 
+import christmas.domain.Event;
 import christmas.domain.VisitDay;
 import christmas.domain.Orders;
+import christmas.service.EventService;
 
 import static christmas.view.output.OutputMessage.INFORMATION_MESSAGE;
 import static christmas.view.output.OutputView.printStaticMessage;
@@ -10,7 +12,8 @@ public class MainController {
     public static void start() {
         printStaticMessage(INFORMATION_MESSAGE);
 
-        VisitDay day = DayController.getVisitDay();
+        VisitDay visitDay = VisitDayController.getVisitDay();
         Orders orders = OrderController.getOrders();
+        Event event = EventService.getEvent(visitDay);
     }
 }
---
+++ src/main/java/christmas/global/constants/EventDays.java
@@ -0,0 +1,23 @@
+package christmas.global.constants;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum EventDays {
+    CHRISTMAS_D_DAY("D-DAY", Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25)),
+    WEEKDAYS("WEEKDAY", Arrays.asList(3,4,5,6,7,10,11,12,13,14,17,18,19,20,21,24,25,26,27,28,31)),
+    WEEKENDS("WEEKEND", Arrays.asList(1,2,8,9,15,16,22,23,29,30)),
+    SPECIAL("SPECIAL", Arrays.asList(3,10,17,24,25,31));
+
+    public List<Integer> getDay() {
+        return day;
+    }
+
+    private final String type;
+    private final List<Integer> day;
+
+    EventDays(String type, List<Integer> day) {
+        this.type = type;
+        this.day = day;
+    }
+}
