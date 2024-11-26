468219733f9e3de6b44c0aad24b20dbe8a014ce1
bmg1203 <bmg1203@naver.com>
2024-11-27 04:10:46 +0900

    [feat]: TotalPrice 클래스 밒 최종 가격 계산 기능 추가

---
+++ src/main/java/christmas/domain/TotalPrice.java
@@ -0,0 +1,23 @@
+package christmas.domain;
+
+public class TotalPrice {
+
+    private final int totalPrice;
+
+    public TotalPrice(Menus menus, Orders orders) {
+        this.totalPrice = calculateTotalPrice(menus, orders);
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    private int calculateTotalPrice(Menus menus, Orders orders) {
+        int sum = 0;
+        for (Order order : orders.getOrders()) {
+            Menu menu = menus.getMenus().get(order.getName());
+            sum += menu.getPrice() * order.getCount();
+        }
+        return sum;
+    }
+}
