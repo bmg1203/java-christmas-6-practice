6ce2099901261812232f81d833a9428e51118a79
bmg1203 <bmg1203@naver.com>
2024-11-27 04:30:14 +0900

    [feat]: OutputPrompts 이벤트별 혜택 금액 출력 상수 추가

--- src/main/java/christmas/constants/OutputPrompts.java
+++ src/main/java/christmas/constants/OutputPrompts.java
@@ -8,7 +8,12 @@ public enum OutputPrompts {
     TOTAL_PRICE_OUTPUT("<할인 전 총주문 금액>\n%s원\n"),
     GIFT_MENU_OUTPUT("<증정 메뉴>\n%s\n"),
     BENEFITS_OUTPUT("<혜택 내역>\n%s\n"),
-    TOTAL_BENEFITS_PRICE_OUTPUT("<총혜택 금액>\n%s원\n"),
+    CHRISTMAS_SALE_PRICE_OUTPUT("크리스마스 디데이 할인: -%s원"),
+    WEEKDAY_SALE_PRICE_OUTPUT("평일 할인: -%s원"),
+    WEEKEND_SALE_PRICE_OUTPUT("주말 할인: -%s원"),
+    SPECIAL_SALE_PRICE_OUTPUT("특별 할인: -%s원"),
+    GIFT_EVENT_PRICE_OUTPUT("증정 이벤트: -%s원"),
+    TOTAL_BENEFITS_PRICE_OUTPUT("<총혜택 금액>\n-%s원\n"),
     FINAL_PAYMENT_PRICE_OUTPUT("<할인 후 예상 결제 금액>\n%s원\n"),
     BADGE_OUTPUT("<12월 이벤트 배지>\n%s원");
 
